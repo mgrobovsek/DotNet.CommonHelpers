a3b54d7863863eab2b7e3c84447297c23554d4c7
Miha Grobovšek <miha.grobovsek@stroka.si>
2020-10-29 11:38:16 +0100

    Add require extension method for elegant condition checks

---
+++ src/DotNet.CommonHelpers/Extensions/CleanerCodeExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DotNet.CommonHelpers.Extensions
+{
+    public static class CleanerCodeExtensions
+    {
+        /// <summary>
+        /// Checks if the parameter meets the condition. It throws an exception if it does not
+        /// </summary>
+        /// <example lang="csharp">
+        /// int amount = 30;
+        /// string productName = "";
+        /// amount.Require(a=>a>0);
+        /// productName.Require(p=>!string.IsNullOrWhiteSpace(p));
+        /// </example>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="param">parameter to be checked</param>
+        /// <param name="condition">Condition to be met</param>
+        public static void Require<T>(this T param, Func<T, bool> condition)
+        {
+            if (!condition(param))
+            {
+                throw new ArgumentException();
+            }
+        }
+    }
+}
