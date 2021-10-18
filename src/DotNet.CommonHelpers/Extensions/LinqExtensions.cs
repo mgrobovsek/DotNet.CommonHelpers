ecf844b97aeac8ba127e222dd3ca1ccf1d972650
miha.grobovsek <miha.grobovsek@gmail.com>
2021-10-18 23:16:37 +0200

    Add maxindex and ploting ienumerable to console

--- src/DotNet.CommonHelpers/Extensions/LinqExtensions.cs
+++ src/DotNet.CommonHelpers/Extensions/LinqExtensions.cs
@@ -17,5 +17,35 @@ namespace DotNet.CommonHelpers.Extensions
         {
             return listToClone.Select(item => (T)item.Clone()).ToList();
         }
+
+        /// <summary>
+        /// Index of max value
+        /// </summary>        
+        public static int MaxIndex<T>(this IEnumerable<T> sequence) where T : IComparable<T>
+        {
+            int maxIndex = -1;
+            T maxValue = default; // Immediately overwritten anyway
+
+            int index = 0;
+            foreach (T value in sequence)
+            {
+                if (value.CompareTo(maxValue) > 0 || maxIndex == -1)
+                {
+                    maxIndex = index;
+                    maxValue = value;
+                }
+                index++;
+            }
+            return maxIndex;
+        }
+
+        /// <summary>
+        /// Write items to console using converter function func
+        /// </summary>
+        public static void ToConsoleSameLine<T>(this IEnumerable<T> @this, Func<T, string> func)
+        {
+            foreach (T item in @this)
+                Console.Write(func(item));
+        }
     }
 }
