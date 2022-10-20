48d96d33a207eae7765a7533feff9c6b0b63e51d
Miha Grobovšek <Miha.Grobovsek@stroka.si>
2022-10-21 00:19:14 +0200

    Add descending range feature

--- src/DotNet.CommonHelpers/Extensions/CustomIntEnumerator.cs
+++ src/DotNet.CommonHelpers/Extensions/CustomIntEnumerator.cs
@@ -17,31 +17,64 @@ namespace DotNet.CommonHelpers.Extensions
         /// </code>
         /// </example>
         public static CustomIntEnumerator GetEnumerator(this Range range)
-        {
-            return new CustomIntEnumerator(range);
+        {            
+            return new CustomIntEnumerator(range);            
         }
 
     }
 
-    public ref struct CustomIntEnumerator
+    public class CustomIntEnumerator
     {
         private int _current;
         private readonly int _end;
+        private MoveNextDelegate _moveNextAction;
 
+        delegate  bool  MoveNextDelegate();
         public CustomIntEnumerator(Range range)
         {
+
             if (range.End.IsFromEnd)
                 throw new Exception("End of interval must be specified!");
 
-            _current = range.Start.Value - 1;
-            _end = range.End.Value;
+            
+
+            if (range.End.Value > range.Start.Value)
+            {
+
+                _current = range.Start.Value - 1;
+                _end = range.End.Value;
+                _moveNextAction = new MoveNextDelegate(MoveNextInc);
+            }
+            else
+            {
+                //if (range.Start.IsFromEnd)
+                //    throw new Exception("End of interval must be specified!");
+                //_current = range.End.Value;
+                //_end = range.Start.Value - 1;
+
+                _current = range.Start.Value+1;
+                _end = range.End.Value;
+                _moveNextAction = new MoveNextDelegate(MoveNextDec);                
+            }
+
         }
 
+        
+
         public int Current => _current;
-        public bool MoveNext()
+        public bool MoveNext() => _moveNextAction();
+        
+        private bool MoveNextInc()
         {
             _current++;
             return _current <= _end;
         }
+        private bool MoveNextDec()
+        {
+            _current--;
+            return _current >= _end;
+        }
+
     }
+   
 }
